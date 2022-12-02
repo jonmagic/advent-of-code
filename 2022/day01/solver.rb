96131ba2ba2d72d30ceec75e2863291966e204c2
Jonathan Hoyt <jonmagic@gmail.com>
2022-12-01 20:49:28 -0800

    Answer for day 1 puzzle 2

--- 2022/day01/solver.rb
+++ 2022/day01/solver.rb
@@ -1,7 +1,7 @@
 data = File.readlines("input.csv")
 calories_by_elf = Hash.new(0)
 elf = 1
-most_calories = 0
+most_calories = []
 data.each do |row|
   if row == "\n"
     elf += 1
@@ -9,9 +9,10 @@ data.each do |row|
     calories_by_elf[elf] += row.to_i
   end
 
-  if most_calories < calories_by_elf[elf]
-    most_calories = calories_by_elf[elf]
-  end
+  most_calories.push(calories_by_elf[elf])
 end
+most_calories.sort!
 puts calories_by_elf
-puts "\nMost calories: #{most_calories}"
+puts "\nMost calories: #{most_calories[-1]}"
+
+puts "\nSum of calories from three elves with most calories: #{most_calories[-3..-1].sum}"
