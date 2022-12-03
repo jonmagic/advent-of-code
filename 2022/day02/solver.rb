a8ba29a6e06506886a51e4d4c01863c8b0417091
Jonathan Hoyt <jonmagic@gmail.com>
2022-12-03 10:19:02 -0800

    Answer for day 2 puzzle 2

--- 2022/day02/solver.rb
+++ 2022/day02/solver.rb
@@ -9,6 +9,14 @@
 # loss, score = 0
 # draw, score = 3
 
+def should_log
+  false
+end
+
+def log(value)
+  puts value if should_log
+end
+
 mappings = {
   "A" => "rock",
   "X" => "rock",
@@ -23,14 +31,6 @@ mappings = {
   "draw" => 3,
   "win" => 6,
 }
-
-def should_log
-  false
-end
-
-def log(value)
-  puts value if should_log
-end
 
 score = 0
 File.readlines("input.csv").each do |line|
@@ -55,4 +55,62 @@ File.readlines("input.csv").each do |line|
   log "points_from_result: #{points_from_result}"
   score += points_from_result
 end
-puts "My score: #{score}"
+puts "Puzzle 1 score: #{score}"
+
+mappings = {
+  "A" => "rock",
+  "X" => "loss",
+  "B" => "paper",
+  "Y" => "draw",
+  "C" => "scissor",
+  "Z" => "win",
+  "rock" => 1,
+  "paper" => 2,
+  "scissor" => 3,
+  "loss" => 0,
+  "draw" => 3,
+  "win" => 6,
+}
+
+score = 0
+File.readlines("input.csv").each do |line|
+  line = line.gsub(/\n$/, "")
+  log "line: #{line}"
+  opponent, result = line.split(" ").map { |key| mappings[key] }
+
+  case opponent
+  when "rock"
+    if result == "win"
+      shape = "paper"
+    elsif result == "loss"
+      shape = "scissor"
+    else
+      shape = "rock"
+    end
+  when "paper"
+    if result == "win"
+      shape = "scissor"
+    elsif result == "loss"
+      shape = "rock"
+    else
+      shape = "paper"
+    end
+  when "scissor"
+    if result == "win"
+      shape = "rock"
+    elsif result == "loss"
+      shape = "paper"
+    else
+      shape = "scissor"
+    end
+  end
+  points_from_shape = mappings[shape]
+  log "points_from_shape: #{points_from_shape}"
+  score += points_from_shape
+
+  points_from_choice = mappings[result]
+  log "points_from_choice: #{points_from_choice}"
+  score += points_from_choice
+end
+puts "Puzzle 2 score: #{score}"
+
